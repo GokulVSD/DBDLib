f7bdd5a65f9e63b249d12e3e61be8d0beab25f1c
Gokul Vasudeva <gokul.vasda@gmail.com>
2018-03-04 15:21:19 +0530

    Revert "Prototyping functions"

--- src/dbdatabase/customer/CustomerCreator.java
+++ src/dbdatabase/customer/CustomerCreator.java
@@ -4,12 +4,8 @@ import dbdatabase.FileCreator;
 
 public class CustomerCreator extends FileCreator {
 
-    public CustomerCreator(String customerID) //calls super's constructor with customerID + ".txt", then calls initCustomerStructure,
-    // then calls super's createFile. checks if Index file exists, if not, creates it by passing customerID to IndexCreator.
-    // if Index exists, Adds entry with customer status to Index with IndexEntry
-    {
+    public CustomerCreator(String customerID){
 
         super(customerID + ".txt");
     }
-    void initCustomerStructure(); //edits super's content variable
 }
