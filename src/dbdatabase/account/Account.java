cbb9913b6163b8b5a67333341b3698702ab81642
GokulVSD <gokul.vasda@gmail.com>
2018-03-04 16:24:35 +0530

    Revert "Revert "Prototyping functions""
    This reverts commit f7bdd5a

--- src/dbdatabase/account/Account.java
+++ src/dbdatabase/account/Account.java
@@ -10,9 +10,18 @@ import dbdatabase.customer.Customer;
 public class Account extends Customer {
 
     protected String accountNo;
+    protected String account;
 
-    public Account(String accountNo){
+    public Account(String accountNo) throws Exception //gets customerID from index, and passes to super. calls super's getAccount//
+    {
 
         this.accountNo = accountNo;
     }
+
+    String getAccountDetail(String key);
+    void appendAccountLog(String log);
+    void save(); //calls super's writeAccount, then calls super's close
+    void close(); //calls super's close
+
+
 }
--- src/dbdatabase/customer/CustomerCreator.java
+++ src/dbdatabase/customer/CustomerCreator.java
@@ -4,8 +4,12 @@ import dbdatabase.FileCreator;
 
 public class CustomerCreator extends FileCreator {
 
-    public CustomerCreator(String customerID){
+    public CustomerCreator(String customerID) //calls super's constructor with customerID + ".txt", then calls initCustomerStructure,
+    // then calls super's createFile. checks if Index file exists, if not, creates it by passing customerID to IndexCreator.
+    // if Index exists, Adds entry with customer status to Index with IndexEntry
+    {
 
         super(customerID + ".txt");
     }
+    void initCustomerStructure(); //edits super's content variable
 }
