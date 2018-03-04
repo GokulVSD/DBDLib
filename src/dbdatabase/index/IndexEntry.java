cd423f279a9b01c88b0828387117b5a8055e4c2d
GokulVSD <gokul.vasda@gmail.com>
2018-03-04 16:23:03 +0530

    Revert "Prototyping (#4)"
    This reverts commit 2ebab6a

--- src/dbdatabase/index/IndexEntry.java
+++ src/dbdatabase/index/IndexEntry.java
@@ -2,18 +2,8 @@ package dbdatabase.index;
 
 public class IndexEntry extends Index {
 
-    protected String entry;
-    protected String customerID;
+    public IndexEntry(String customerID){
 
-    public IndexEntry(String customerID)throws Exception{
         super();
-    } //calls super's constructor, then calls super's getCustomerEntry. if it returns null, then {calls super's writeCustomerEntry,then calls super's close}
-
-    void addAccount(String accountNo); //adds account to entry, and sets its status to open
-    void closeAccount(String accountNo);
-    void reopenAccount(String accountNo);
-    void activateCustomer();
-    void deactivateCustomer();
-    void save(); //calls super's writeCustomerEntry, then calls super's close
-
+    }
 }
