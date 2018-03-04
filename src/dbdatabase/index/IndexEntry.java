fd3f7f08e8877dcbeddd1a4f569fd7766d547097
GokulVSD <gokul.vasda@gmail.com>
2018-03-04 16:24:15 +0530

    Revert "Comments"
    This reverts commit 6cb7661

--- src/dbdatabase/index/IndexEntry.java
+++ src/dbdatabase/index/IndexEntry.java
@@ -2,8 +2,19 @@ package dbdatabase.index;
 
 public class IndexEntry extends Index {
 
-    public IndexEntry(String customerID){
+    protected String entry;
+    protected String customerID;
 
+    public IndexEntry(String customerID)throws Exception{
         super();
-    }
+    } //calls super's constructor, then calls super's getCustomerEntry. if it returns null, then {calls super's writeCustomerEntry,
+    // then calls super's close}
+
+    void addAccount(String accountNo); //adds account to entry, and sets its status to open
+    void closeAccount(String accountNo);
+    void reopenAccount(String accountNo);
+    void activateCustomer();
+    void deactivateCustomer();
+    void save(); //calls super's writeCustomerEntry, then calls super's close
+
 }
