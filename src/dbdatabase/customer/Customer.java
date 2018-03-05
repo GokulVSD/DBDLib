d53cc66ef2ae9050dd865a2e9ffd54e76f765134
GokulVSD <gokul.vasda@gmail.com>
2018-03-05 21:02:51 +0530

    Defined customer class' constructor

--- src/dbdatabase/customer/Customer.java
+++ src/dbdatabase/customer/Customer.java
@@ -1,5 +1,7 @@
 package dbdatabase.customer;
 
+import dbdatabase.DB;
+
 /**
  * Customer ID
  * Reads Customer file
@@ -14,15 +16,25 @@ public class Customer {
     protected String details;
     protected String accounts;
 
-    public Customer(String customerID) throws Exception{ //interfaces with DB class, throws exception if customer file is already open. calls readFile//
-
+    /**the DB class is checked to see if the file pertaining to the customerID is
+     * currently open elsewhere; if it is, throws an exception.
+     * If not being used, tells DB class that it's going to use the file, and
+     * calls readFile().
+     * @param customerID: customerID for interacting with customer file.
+     * @throws Exception: if file is already open elsewhere.
+     */
+    public Customer(String customerID) throws Exception {
+        if(DB.isCustomerBeingUsed(customerID))
+            throw new Exception("File already open, please call object.close() or object.save() after using the object");
+        DB.useCustomer(customerID);
+        readFile();
         this.customerID = customerID;
     }
 
-    void readFile(){}
-    String getCustomerDetail(String key){return null;}
-    String getAccount(String accountNo){return null;}
-    String writeAccount(String accountNo,String account){return null;} //if ac exists, replaces, else adds. if account is null, removes account
-    void close(){} //calls writeFile
-    void writeFile(){} //interfaces with DB class
+    private void readFile(){}
+    public String getCustomerDetail(String key){return null;}
+    public String getAccount(String accountNo){return null;}
+    public String writeAccount(String accountNo,String account){return null;} //if ac exists, replaces, else adds. if account is null, removes account
+    public void close(){} //calls writeFile
+    private void writeFile(){} //interfaces with DB class
 }
