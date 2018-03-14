6e959b0d52a568bd701b163f030f55f2f6e2e355
GokulVSD <gokul.vasda@gmail.com>
2018-03-14 17:59:15 +0530

    Defined the IndexEntry class, although it hasn't been tested.

--- src/dbdatabase/index/IndexEntry.java
+++ src/dbdatabase/index/IndexEntry.java
@@ -1,20 +1,45 @@
 package dbdatabase.index;
 
+import java.util.StringTokenizer;
+
+//this class has not been tested
 public class IndexEntry extends Index {
 
-    protected String entry;
-    protected String customerID;
+    private String entry;
+    private String customerID;
 
     public IndexEntry(String customerID)throws Exception{
         super();
-    } //calls super's constructor, then calls super's getCustomerEntry. if it returns null, then {calls super's writeCustomerEntry,
-    // then calls super's close}
+        this.customerID = customerID;
+        entry = super.getCustomerEntry(customerID);
+        if(entry == null){
+            entry = customerID + ":" + "activated" + ":";
+            save();
+        }
+    }
 
-    void addAccount(String accountNo){} //adds account to entry, and sets its status to open
-    void closeAccount(String accountNo){}
-    void reopenAccount(String accountNo){}
-    void activateCustomer(){}
-    void deactivateCustomer(){}
-    void save(){} //calls super's writeCustomerEntry, then calls super's close
+    void addAccount(String accountNo){
+        entry += accountNo + ":" + "open" + ":";
+    }
+
+    void closeAccount(String accountNo){
+        int accountIndex = entry.indexOf(accountNo);
+        if(entry.contains(accountNo))
+            entry = entry.substring(0,accountIndex) + entry.substring(accountIndex).replaceFirst("open","closed");
+    }
+    void reopenAccount(String accountNo){
+        int accountIndex = entry.indexOf(accountNo);
+        entry = entry.substring(0,accountIndex) + entry.substring(accountIndex).replaceFirst("open","closed");
+    }
+    void activateCustomer(){
+        entry = entry.replaceFirst("deactivated","activated");
+    }
+    void deactivateCustomer(){
+        entry = entry.replaceFirst("activated","deactivated");
+    }
+    void save(){
+        super.writeCustomerEntry(customerID,entry);
+        super.close();
+    }
 
 }
