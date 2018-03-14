72a523eb0bbade0307285ce6a143f712049c1ff8
GokulVSD <gokul.vasda@gmail.com>
2018-03-14 20:38:20 +0530

    Defined Acccount class, although it hasn't been tested.

--- src/dbdatabase/account/Account.java
+++ src/dbdatabase/account/Account.java
@@ -1,21 +1,38 @@
 package dbdatabase.account;
 
 import dbdatabase.customer.Customer;
+import dbdatabase.index.Index;
 
+import java.util.StringTokenizer;
+
+//this class has not been tested
 public class Account extends Customer {
 
     protected String accountNo;
     protected String account;
 
-    public Account(String accountNo) throws Exception { //gets customerID from index, and passes to super. calls super's getAccount//
-        super(null);
+    public Account(String accountNo) throws Exception {
+        super(new Index().getCustomerID(accountNo));
         this.accountNo = accountNo;
+        account = super.getAccount(accountNo);
     }
 
-    String getAccountDetail(String key){return null;}
-    void appendAccountLog(String log){}
-    void save(){} //calls super's writeAccount, then calls super's close
-    public void close(){} //calls super's close //not needed
-
-
+    public String getAccountDetail(String key){
+        String accDetails = new StringTokenizer(account,"!").nextToken();
+        StringTokenizer st = new StringTokenizer(accDetails,",");
+        while (st.hasMoreTokens()){
+            StringTokenizer pair = new StringTokenizer(st.nextToken(),"=");
+            if(pair.nextToken().equals(key)) return pair.nextToken();
+        }
+        return null;
+    }
+    public void appendAccountLog(String log){
+        account = account.substring(0,account.lastIndexOf("!"));
+        account += log + "," + "!";
+    }
+    public void save(){
+        super.writeAccount(accountNo,account);
+        super.close();
+    }
 }
+
